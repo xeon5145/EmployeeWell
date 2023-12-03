f3bed5a228d39b520c4420a938168f9383467054
Abhishek Kagra <Abhishekkagra@aaddoo.net>
2023-12-03 12:56:56 +0530

    fetched all users from db

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,11 @@ require_once "config.php";
 session_start();
 $userid = $_SESSION['loggedInUser'];
 
+// getting all users in the system
+$getUsers = "SELECT u.id,ud.empid,u.firstname,u.lastname,u.email FROM emp_users as u JOIN emp_user_data as ud ON u.id = ud.userid WHERE deleted = 0";
+$resGU = getSelect($conn,$getUsers,'array');
+$rowGU = count($resGU);
+// getting all users in the system
 include "navbar.php";
 include "views/dashboard.php";
 ?>
